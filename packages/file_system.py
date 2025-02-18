91dfd424f4783806ce3b46c25b42e5809963456f
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-18 23:50:31 +0200

    style: add comments

--- packages/file_system.py
+++ packages/file_system.py
@@ -48,13 +48,14 @@ def getSoundDuration(random_number):
 	return math.ceil(float(audio_info['duration'])) + 10
 
 
-# Play azan
+# Play azan, here we need a random number for azan when function called then call playFile
 def playAzan(is_sobuh_now=False):
 	random_number_for_azan = general.generateRandomNumber(1,3)
 	file_path = f"./azan/azan_{random_number_for_azan}.mp3"
 	playFile(file_path=file_path,is_sobuh_now=is_sobuh_now)
 
-# Play quran
+# Play azan, here we need a random number for quran to specify path function called then call
+# playFile
 def playQuran(random_number,is_sobuh_now=False):
 	file_path = f"./quran/quran_{random_number}.mp3"
 	playFile(file_path=file_path,is_sobuh_now=is_sobuh_now)
@@ -72,6 +73,7 @@ def playSound(random_number,is_adan_and_quran=False,azan_time=None,is_sobuh_now=
 	time.sleep(1)
 	general.turnIzaa(is_from_mic=False,is_to_on=False)
 
+# Play sound from file, needed only path and if is_sobuh to decrease volume
 def playFile(file_path,is_sobuh_now=False):
 	sound = AudioSegment.from_file(file_path)
 	if is_sobuh_now:
